4af70bdfe1e5cd3bf07a390c4b688ae24c45804f
Richard Collins <r.p.collins@sheffield.ac.uk>
2019-09-18 17:17:11 +0100

    Starting to add in the epanet controls

---
+++ Core/epanet_integration.py
@@ -0,0 +1,13 @@
+import epanettools.epanettools as epa
+
+
+def epanet_import(epainputfile,network = None):
+    ### This function will import an epanet .inp file and output the data into the hyd_Trans network file type
+    ### It requires the input filename, and a network
+    es = epa.EPANetSimulation(epainputfile)
+    noNodes = len(es.network.nodes)
+    noLinks = len(es.network.links)
+    print("There are ", noNodes, " in the network")
+    print("There are ", noLinks, " in the network")
+
+
