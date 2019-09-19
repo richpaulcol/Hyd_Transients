58246aad4d6322c22e56aa78f18a06543b4b0158
Richard Collins <r.p.collins@sheffield.ac.uk>
2019-09-19 20:55:08 +0100

    a couple of quick notes about saving and reloading a project

--- Core/project.py
+++ Core/project.py
@@ -3,8 +3,11 @@ import Elements
 import Models
 import Solvers
 import Utilties
+import h5py
 
 ### Saving a project
+# I am going to use the hdf5 file type as a persistence storage approach.
+# I am also wondering about the pickle or shelve formats for saving information.
 
 ### Loading a project
 
